4f7b0281ae41c033c04f68dbfa6c7533198501f8
imcybergeek <imcybergeek@gmail.com>
2022-06-26 22:06:07 +0530

    dynamic images added

--- src/App.js
+++ src/App.js
@@ -6,35 +6,35 @@ function App() {
 
   const data = [
     {
-      img:"",
+      img:["https://5.imimg.com/data5/SELLER/Default/2022/4/PF/IE/LP/146060024/new-nord-stage-3-88-piano-fully-weighted-hammer-action-keyboard-digital-piano-500x500.jpg"],
       name: "Nord Stage 3 88 Keys",
       tags:[{text:"Brand New",color:"lightcoral"}, {text:"Free Shipping",color:"lightblue"}, {text:"15% Off",color:"lightpink"}],
       location: ["EMLA Music","Le Havre, France",""],
       pricing: ["6500.00","5,738.00","195"],
     },
     {
-      img:"",
+      img:"https://cdn.shopaccino.com/johns-music/products/1-716340_l.jpg",
       name: "Line 6 Helix Rack",
       tags:[{text:"Brand New",color:"lightcoral"}],
       location: ["ProMusicTools","Munchen, Germany",""],
       pricing: ["","1,831.57","244.36"],
     },
     {
-      img:"",
+      img:"https://audiogears.in/wp-content/uploads/2020/07/61X9Mmv99L._SL1200_.jpg",
       name: "Nord Lead A1",
       tags:[{text:"Brand New",color:"lightcoral"}, {text:"Free Shipping",color:"lightblue"}],
       location: ["Gear Garage","Dulwich Hill, Australia",""],
       pricing: ["","1700","35"],
     },
     {
-      img:"",
+      img:"https://cdn.shopaccino.com/johns-music/products/1503917131img857185-396421_l.jpg",
       name: "Mong Subsequent 37",
       tags:[{text:"Free Shipping",color:"lightblue"}],
       location: ["TURNLAB","Antwerpen, Belgium"],
       pricing: ["","1700","205"],
     },
     {
-      img:"",
+      img:"https://cdn.shopaccino.com/johns-music/products/13830658761010608-547250_l.jpg",
       name: "Mong Miniature",
       tags:[{text:"Brand New",color:"lightcoral"}, {text:"Free Shipping",color:"lightblue"}, {text:"15% Off",color:"lightpink"}],
       location: ["Andrew's Gear Outlet","Ojai CA, United States"],
--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import Tag from './Tag';
 let Container = ({data}) => {
     return <>
     {data.map((curr)=><div className='container'>
-        <img src="https://m.media-amazon.com/images/I/611zFJPgriL._SL1200_.jpg"></img>
+        <img src={curr.img}></img>
         <div className='subcontainer'>
             <h3>{curr.name}</h3>
             <div className='tags'>
@@ -10,11 +10,11 @@ let Container = ({data}) => {
             <div style={{background:tag.color, height: "50px", textAlign:"center", lineHeight:"20px", paddingLeft:"5px", paddingRight:"5px"}}>{tag.text}</div>)}
             </div>
         </div>
-        <div className='location' style={{paddingLeft:"600px", position:"absolute"}}>
+        <div className='location' style={{paddingLeft:"35rem", position:"absolute"}}>
             <p>{curr.location[0]}</p>
             <p>{curr.location[1]}</p>
         </div>
-        <div className='pricing' style={{paddingLeft:"1000px", position:"absolute"}}>
+        <div className='pricing' style={{paddingLeft:"60rem", position:"absolute"}}>
             <p style={{color:"red"}}><s>{curr.pricing[0]!==""?`$${curr.pricing[0]}`:''}</s></p>
             <p>{`$${curr.pricing[1]} AUD`}</p>
             <p>{`+${curr.pricing[2]} Shipping`}</p>
