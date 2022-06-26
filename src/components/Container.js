6a2480a26f76bebc7d899970f941bb518d7ac558
imcybergeek <imcybergeek@gmail.com>
2022-06-26 23:24:53 +0530

    final

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,34 +1,70 @@
-import Tag from './Tag';
-let Container = ({data}) => {
-    return <>
-    {data.map((curr)=><div className='container'>
-        <img src={curr.img}></img>
-        <div className='subcontainer'>
+let Container = ({ data }) => {
+  return (
+    <>
+      {data.map((curr) => (
+        <div className="container">
+          <img src={curr.img} alt="Please enable internet to see images"></img>
+          <div className="subcontainer">
             <h3>{curr.name}</h3>
-            <div className='tags'>
-            {curr.tags.map((tag)=>
-            <div style={{background:tag.color, height: "50px", textAlign:"center", lineHeight:"20px", paddingLeft:"5px", paddingRight:"5px"}}>{tag.text}</div>)}
+            <div className="tags">
+              {curr.tags.map((tag) => (
+                <div
+                  style={{
+                    background: tag.color,
+                    textAlign: "center",
+                    lineHeight: "20px",
+                    padding: "5px",
+                    paddingLeft: "10px",
+                    paddingRight: "10px",
+                    marginLeft: "10px",
+                    borderRadius: 5,
+                  }}
+                >
+                  {tag.text}
+                </div>
+              ))}
             </div>
-        </div>
-        <div className='location' style={{paddingLeft:"35rem", position:"absolute", lineHeight:"5px"}}>
+          </div>
+          <div
+            className="location"
+            style={{
+              paddingLeft: "35rem",
+              position: "absolute",
+              lineHeight: "5px",
+            }}
+          >
             <p>{curr.location[0]}</p>
             <p>{curr.location[1]}</p>
-            <div style={{display:"flex", alignItems:"center"}}>{[...Array(5)].map(()=><img src = "https://icon-library.com/images/star-rating-icon/star-rating-icon-4.jpg" style={{height:"15px", width:"15px"}}></img>)}
-            <p style={{paddingLeft:"5px"}}>{`(${curr.review})`}</p></div>
-        </div>
-        <div className='pricing' style={{paddingLeft:"60rem", position:"absolute"}}>
-            <p style={{color:"red"}}><s>{curr.pricing[0]!==""?`$${curr.pricing[0]}`:''}</s></p>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              {[...Array(5)].map(() => (
+                <img
+                  src="https://icon-library.com/images/star-rating-icon/star-rating-icon-4.jpg"
+                  style={{ height: "15px", width: "15px" }}
+                  alt="*"
+                ></img>
+              ))}
+              <p style={{ paddingLeft: "5px" }}>{`(${curr.review})`}</p>
+            </div>
+          </div>
+          <div
+            className="pricing"
+            style={{ paddingLeft: "60rem", position: "absolute" }}
+          >
+            <p style={{ color: "red" }}>
+              <s>{curr.pricing[0] !== "" ? `$${curr.pricing[0]}` : ""}</s>
+            </p>
             <p>{`$${curr.pricing[1]} AUD`}</p>
             <p>{`+${curr.pricing[2]} Shipping`}</p>
-
-        </div>
-        <div className='buttons'>
+          </div>
+          <div className="buttons">
             <button>Add To Cart</button>
             <br></br>
             <button>Make Offer</button>
+          </div>
         </div>
-    </div>)}
+      ))}
     </>
-}
+  );
+};
 
-export default Container
+export default Container;
--- src/components/Tag.js
+++
@@ -1,5 +0,0 @@
-let Tag = () => {
-    return <>
-    <div style={{height: "10px", width: "10px", background: "red"}}></div>
-    </>
-}
